52608107302a971339ba2ad29e713c24fc69c5ce
Kevin Barrera <kbarrera@estud.usfq.edu.ec>
2026-06-30 11:03:25 -0500

    Add hello function to greet user by name

--- seccion 4.1.py
+++ seccion 4.1.py
@@ -1,6 +1,18 @@
+'''
 def message():    # definiendo una función
     print("Hello")    # cuerpo de la función
 
 message()    # invocación de la función
+'''
+
+def hello(name):    # definiendo una función
+    print("Hello,", name)    # cuerpo de la función
+
+
+name = input("Ingresa tu nombre: ")
+
+hello(name)    # invocación de la función
+
+
 
 
